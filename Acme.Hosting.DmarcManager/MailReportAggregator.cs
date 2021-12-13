ad287902a807bd440503b525761190de9ec556ea
Simon Baudart <s.baudart@wavenet.be>
2021-12-13 15:31:35 +0100

    feat: Add the Time Trigger function

--- Acme.Hosting.DmarcManager/MailReportAggregator.cs
+++ Acme.Hosting.DmarcManager/MailReportAggregator.cs
@@ -7,14 +7,25 @@ namespace Acme.Hosting.DmarcManager;
 using System;
 using System.Threading.Tasks;
 
+using Acme.Hosting.Dmarc.Tools.Abstractions;
+
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 
-public static class MailReportAggregator
+public class MailReportAggregator
 {
+    private readonly IPop3Aggregator pop3Aggregator;
+
+    public MailReportAggregator(IPop3Aggregator pop3Aggregator)
+    {
+        this.pop3Aggregator = pop3Aggregator;
+    }
+
     [FunctionName("MailReportAggregator")]
-    public static async Task RunAsync([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer, ILogger log)
+    public async Task RunAsync([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer, ILogger log)
     {
         log.LogInformation("MailReportAggregator executed by a http trigger function at: {date}", DateTime.UtcNow);
+
+        await this.pop3Aggregator.ExecuteAsync();
     }
 }
